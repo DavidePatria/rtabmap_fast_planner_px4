f23d2d3bb57341a773438d181a08c9f85a1231b7
Davide <davide.patria@gmail.com>
2022-03-31 16:37:56 +0200

    add class with methods for service server

--- src/takeoff_custom_srv_server.cpp
+++ src/takeoff_custom_srv_server.cpp
@@ -1,17 +1,37 @@
 #include "ros/ros.h"
-#include "rtabmap_drone_example/MakeTakeoff.h"
+#include "offboard_safety/MakeTakeoff.h"
+#include "ros/time.h"
+#include "ros/subscriber.h"
+#include "std_msgs/Bool.h"
+
+class TeichingOfServis {
+	public:
+		TeichingOfServis() {
+			pub_land = nh.advertise<std_msgs::Bool>("land_stream", 1);
+		}
+		
+		bool send_takeoff(offboard_safety::MakeTakeoff::Request &req, offboard_safety::MakeTakeoff::Response &res) {
+			pub_land.publish(&req);
+			return true;
+		};
+		
+	private:
+		ros::NodeHandle nh;
+		ros::Subscriber sub_land;
+		ros::Publisher pub_land;
+		// el constructor tendrá los subscriptores
+};
 
 
-
-
-bool send_takeoff(MakeTakeoff::MakeTakeoff.srv::Request &req, MakeTakeoff::MakeTakeoff::Response &res) {
-}
 
 int main(int argc, char **argv) {
+
+	TeichingOfServis teicoff;
+
 	ros::init(argc, argv, "custom_takeoff");
 	ros::NodeHandle nh;
 
-	ros::ServiceServer service = nh.advertiseService("custom/make_takeoff", send_takeoff);
+	ros::ServiceServer service = nh.advertiseService("custom/make_takeoff", &TeichingOfServis::send_takeoff, &teicoff);
 
 	ROS_INFO("make_takeoff server has started");
 	ros::spin();
