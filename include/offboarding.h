2cb8172e6b5a437ecfbfb9e5900b9c1a2a7e429a
Davide <davide.patria@gmail.com>
2022-04-01 15:19:51 +0200

    add all parameters and functions to source and header

--- include/offboarding.h
+++ include/offboarding.h
@@ -1,13 +1,14 @@
 #ifndef OFFBOARDING_H
 #define OFFBOARDING_H
 
-#include "ros/ros.h"
-#include "std_msgs/Empty.h"
+#include "ros/publisher.h"
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
 
 #include <mavros_msgs/State.h>
 #include <mavros_msgs/PositionTarget.h>
 
-
+#include <std_msgs/Empty.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 
@@ -18,8 +19,14 @@
 class OffBoarding {
 public:
 	OffBoarding();
+	~OffBoarding();
 	bool is_beat_fresh();
 	void set_beat();
+	void updatePose(geometry_msgs::PoseStamped &pose, const tf::StampedTransform &vision);
+	void setPosGoal(mavros_msgs::PositionTarget &goal, geometry_msgs::PoseStamped &pose );
+
+	ros::Publisher local_pos_pub;
+	ros::Publisher vision_pos_pub;
 
 private:
 
@@ -27,19 +34,20 @@ private:
 
 	// state tracking variables
 	bool was_flying_;
-	ros::Time letItDoItsThing_;
-	mavros_msgs::PositionTarget current_goal_;
-	mavros_msgs::State current_state_;
-	ros::Time lastTwistReceived_;
-	ros::Time lastRemoteBeat_;
 	bool donotprint_ = false;
 	bool a_prem_;
 
+	mavros_msgs::PositionTarget current_goal_;
+	mavros_msgs::State current_state_;
+
+	ros::Time letItDoItsThing_;
+	ros::Time lastTwistReceived_;
+	ros::Time lastRemoteBeat_;
 
 	ros::NodeHandle nh_;
 
-	ros::Subscriber remote_pub_;
-	ros::Subscriber state_pub_;
+	ros::Subscriber remote_sub_;
+	ros::Subscriber state_sub_;
 	ros::Subscriber twist_sub_;
 	ros::Subscriber joy_sub_;
 
--- src/offboarding.cpp
+++ src/offboarding.cpp
@@ -1,8 +1,32 @@
 // #include "ros/subscriber.h"
 // #include "ros/time.h"
 
-#include "offboarding.h"
-#include "ros/time.h"
+#include "ros/init.h"
+#include <offboarding.h>
+// #include "ros/time.h"
+
+OffBoarding::OffBoarding():nh_("") {
+
+	ROS_INFO("Class istantiatied. Creatind subscribers");
+	// short queue. to 1. might cause problems bu the topic type allows it
+	remote_sub_ = nh_.subscribe("/remote/beat", 1,
+						   &OffBoarding::remote_cb_, this);
+	state_sub_ = nh_.subscribe("mavros/state", 10,
+						   &OffBoarding::state_cb_, this);
+	twist_sub_ = nh_.subscribe("/box_bot/imu/data3", 10,
+						   &OffBoarding::remote_cb_, this);
+	joy_sub_ = nh_.subscribe("/box_bot/imu/data3", 10,
+						   &OffBoarding::joy_cb_, this);
+	
+	local_pos_pub = nh_.advertise<mavros_msgs::PositionTarget>
+	("mavros/setpoint_raw/local", 1);
+	vision_pos_pub = nh_.advertise<geometry_msgs::PoseStamped>
+	("mavros/vision_pose/pose", 1);
+	
+	// ROS_INFO("Init Finished");
+}
+
+OffBoarding::~OffBoarding() { ros::shutdown(); }
 
 void OffBoarding::state_cb_(const mavros_msgs::State::ConstPtr& msg) {
 	current_state_ = *msg;
@@ -16,10 +40,10 @@ void OffBoarding::twist_cb_(const geometry_msgs::Twist::ConstPtr& msg) {
 			ROS_INFO("Switch to velocity control");
 		}
 		current_goal_.coordinate_frame = mavros_msgs::PositionTarget::FRAME_BODY_NED;
-		current_goal_.type_mask = velocity_mask;
+		current_goal_.type_mask = velocity_mask_;
 		current_goal_.velocity.x = msg->linear.x;
 		current_goal_.velocity.y = msg->linear.y;
-		current_goal_.velocity.z = velocity_mask == VELOCITY2D_CONTROL?0:msg->linear.z;
+		current_goal_.velocity.z = velocity_mask_ == VELOCITY2D_CONTROL?0:msg->linear.z;
 		current_goal_.position.z = 1.5;
 		current_goal_.yaw_rate = msg->angular.z;
 		current_goal_.yaw_rate = msg->angular.z;
@@ -35,9 +59,9 @@ void OffBoarding::joy_cb_(const sensor_msgs::Joy::ConstPtr& msg) {
 
 	if(msg->buttons[5] == 1)
 		// When holding right trigger, accept velocity in Z
-		velocity_mask = VELOCITY_CONTROL;
+		velocity_mask_ = VELOCITY_CONTROL;
 	else
-		velocity_mask = VELOCITY2D_CONTROL;
+		velocity_mask_ = VELOCITY2D_CONTROL;
 }
 
 void OffBoarding::remote_cb_(const std_msgs::Empty::ConstPtr& msg) {
@@ -57,3 +81,5 @@ bool OffBoarding::is_beat_fresh() {
 void OffBoarding::set_beat() {
 	lastRemoteBeat_ = ros::Time().now();
 }
+
+
