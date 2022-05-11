4243551142244fd35d850e4bda01073aab75e619
Davide <davide.patria@gmail.com>
2022-05-11 18:16:46 +0200

    correct pose publishing programme

--- offboard_safety/src/vision_pose.cpp
+++ offboard_safety/src/vision_pose.cpp
@@ -1,9 +1,15 @@
 #include "geometry_msgs/PoseStamped.h"
+#include "mavros_msgs/State.h"
 #include "ros/init.h"
 #include "ros/rate.h"
 #include "ros/time.h"
 #include "tf/transform_listener.h"
 
+mavros_msgs::State current_state;
+
+void state_cb(const mavros_msgs::State::ConstPtr& msg) {
+	current_state = *msg;
+}
 
 int main(int argc, char **argv)
 {
@@ -12,32 +18,41 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "pos_publisher");
 
 	ros::Publisher vision_pos_pub;
+	ros::Subscriber state_sub;
 	ros::NodeHandle nh;
 
 	vision_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
 		("mavros/vision_pose/pose", 1);
+	state_sub = nh.subscribe("/mavros/state", 10, state_cb);
 
-	ros::Rate rate(50.0);
+	ros::Rate rate(30.0);
 
 	geometry_msgs::PoseStamped current_pose;
 	current_pose.header.frame_id = "map";
 
 	tf::TransformListener listener;
 
+	while(ros::ok() && !current_state.connected){
+		ROS_INFO("not connected yet!");
+		ros::spinOnce();
+		rate.sleep();
+	}
+
 	ros::spinOnce();
 
-	if(!listener.waitForTransform("/map", "/base_link", ros::Time().now(), ros::Duration(5))) {
-		ROS_ERROR("Cannot get current position between /map and /base_link");
-		return -1;
-	}
+	// if(!listener.waitForTransform("/map", "/base_link", ros::Time().now(), ros::Duration(5))) {
+	// 	ROS_ERROR("Cannot get current position between /map and /base_link");
+	// 	return -1;
+	// }
 
 	while(ros::ok()){
 
 		try{
 			tf::StampedTransform visionPoseTf;
-			listener.lookupTransform("/map", "/base_link", ros::Time().now(), visionPoseTf);
+			listener.lookupTransform("/map", "/base_link", ros::Time(0), visionPoseTf);
 
 			//update currentPose
+			current_pose.header.stamp = ros::Time().now();
 			current_pose.pose.position.x    = visionPoseTf.getOrigin().x();
 			current_pose.pose.position.y    = visionPoseTf.getOrigin().y();
 			current_pose.pose.position.z    = visionPoseTf.getOrigin().z();
@@ -45,6 +60,7 @@ int main(int argc, char **argv)
 			current_pose.pose.orientation.y = visionPoseTf.getRotation().y();
 			current_pose.pose.orientation.z = visionPoseTf.getRotation().z();
 			current_pose.pose.orientation.w = visionPoseTf.getRotation().w();
+			ROS_INFO("pose updated");
 		}
 		catch (tf::TransformException & ex){
 			ROS_ERROR("%s",ex.what());
