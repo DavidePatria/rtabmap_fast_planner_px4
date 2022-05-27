e3b998d633bb0e5322b3f9edf60680c6179e5948
Davide <davide.patria@gmail.com>
2022-05-27 12:46:05 +0200

    fixed publishers problems. now it can handle extrapol

--- rtabmap_drone/scripts/camera_pose_publisher.py
+++ rtabmap_drone/scripts/camera_pose_publisher.py
@@ -2,8 +2,8 @@
 import rospy
 
 # Imports
-import tf
-from geometry_msgs.msg import PoseStamped
+import tf2_ros
+from geometry_msgs.msg import PoseStamped, TransformStamped
 
 if __name__ == '__main__':
     rospy.init_node('camera_pose_publisher')
@@ -16,31 +16,43 @@ if __name__ == '__main__':
 
     # base_link rs200_camera
 
+
+# import tf
+    # listener = tf.TransformListener()
+    # listener.lookupTransform('map', 'base_link', rospy.Time.now())
+
+    # message to store the transform
+    trans = TransformStamped()
+
     pose_pub = rospy.Publisher('camera/pose', PoseStamped, queue_size=1)
-    listener = tf.TransformListener()
+    TFbuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(TFbuffer)
 
     rate = rospy.Rate(30.0)
 
+    # while not TFbuffer.can_transform('map', 'camera_link_rot', rospy.Time.now(), rospy.Duration(1.0)):
     while not rospy.is_shutdown():
         try:
-            # (trans,rot) = listener.lookupTransform('/'+parent_frame, '/'+camera_frame, rospy.Time(0))
             # camera_link_rot is solely used to create a transform to publish the right point cloud
-            listener.waitForTransform('map', 'camera_link_rot', rospy.Time.now(), rospy.Duration(0.2))
-            trans,rot = listener.lookupTransform('map', 'camera_link_rot', rospy.Time.now())
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException, tf.Exception) as e:
+            trans = TFbuffer.lookup_transform('map', 'camera_link_rot', rospy.Time.now(), rospy.Duration(1.0))
+        # except using tf and not tf2_ros
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
             rospy.logwarn(e)
+        # except:
+            rate.sleep()
             continue
 
         pose_msg = PoseStamped()
         pose_msg.header.frame_id = 'map'
         pose_msg.header.stamp = rospy.Time.now()
-        pose_msg.pose.position.x = trans[0]
-        pose_msg.pose.position.y = trans[1]
-        pose_msg.pose.position.z = trans[2]
-        pose_msg.pose.orientation.x = rot[0]
-        pose_msg.pose.orientation.y = rot[1]
-        pose_msg.pose.orientation.z = rot[2]
-        pose_msg.pose.orientation.w = rot[3]
+        pose_msg.pose.position.x = trans.transform.translation.x
+        pose_msg.pose.position.y = trans.transform.translation.y
+        pose_msg.pose.position.z = trans.transform.translation.z
+
+        pose_msg.pose.orientation.x = trans.transform.rotation.x
+        pose_msg.pose.orientation.y = trans.transform.rotation.y
+        pose_msg.pose.orientation.z = trans.transform.rotation.z
+        pose_msg.pose.orientation.w = trans.transform.rotation.w
 
         pose_pub.publish(pose_msg)
 
